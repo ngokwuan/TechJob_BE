8a8fefc91d1b078aba258c541bb7336579846478
ngokwuan <nnquan271204@gmail.com>
2025-12-28 00:58:34 +0700

    w8_update cors with fe url

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.use(
     origin: [
       'http://localhost:3000',
       'http://localhost:3001',
-      'https://fe-tech-job-prj-pnei.vercel.app/',
+      'https://fe-tech-job-prj.vercel.app/',
       'https://techjob-be.onrender.com',
     ],
     credentials: true,
